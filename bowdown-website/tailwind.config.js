5f89b6a0618a401251c15e3dc5b0e5604766aff9
Erika Nazwa <erikanazwa222@gmail.com>
2024-11-02 21:40:34 +0700

    perubahan file admin-dashboard.html

--- bowdown-website/tailwind.config.js
+++ bowdown-website/tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./pages/**/*.{html,js}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
